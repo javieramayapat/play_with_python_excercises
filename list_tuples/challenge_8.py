03cfa963b316a10988dd9493e7d2e83cd1239af9
Javier Amaya <Javieramayapat@gmail.com>
2022-03-31 23:17:08 -0600

    [Add] new challenges

---
+++ list_tuples/challenge_8.py
@@ -0,0 +1,41 @@
+def remove_accents_from_a_word(word: str) -> str:
+    """Remove the acents from the word
+    Args:
+        word (str): Word selected
+
+    Returns:
+        str: Word with accents removed
+    """
+    word_without_accent = word.maketrans("áéíóú", "aeiou")
+    return word.translate(word_without_accent)
+
+
+def is_palindrome(string: str) -> bool:
+
+    if type(string) != str:
+        raise TypeError("The value must be a string")
+
+    if len(string) < 1:
+        raise ValueError("A empty string is not acceptable")
+
+    string = string.lower()
+    string = remove_accents_from_a_word(string)
+    string = string.replace(" ", "")
+
+    return string == string[::-1]
+
+
+def run():
+    """
+    Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo.
+    """
+    palindrome_words = ['Amó la paloma', 'ana', 'jose', 'Ojo',
+                        'perro', 'Yo hago yoga hoy', 'Sé verlas al revés', 1, True, None]
+    message = "The word {word_to_check} is a palindrome? - {result}"
+    for word in palindrome_words:
+        result = is_palindrome(word)
+        print(message.format(word_to_check=word, result=result))
+
+
+if __name__ == '__main__':
+    run()
---
+++ test/test_challenge_8.py
@@ -0,0 +1,26 @@
+import unittest
+from list_tuples.challenge_8 import is_palindrome
+
+
+class TestIsPalindrome(unittest.TestCase):
+
+    # check happy path
+    def test_is_palindrome(self):
+        # test palindrome when has a correct palindrome is in uppercase,
+        self.assertAlmostEqual(is_palindrome('Ana'), True)
+        self.assertAlmostEqual(is_palindrome('ojo'), True)
+        # when has accents and spaces
+        self.assertAlmostEqual(is_palindrome('Amó la paloma'), True)
+        self.assertAlmostEqual(is_palindrome('Yo hago yoga hoy'), True)
+        self.assertAlmostEqual(is_palindrome('Sé verlas al revés'), True)
+
+    # test if my function handles improper inputs correctly and raises a value error
+    def test_values(self):
+        self.assertRaises(ValueError, is_palindrome, "")
+
+    # Check if my function handle inproper inputs correctly and raises a value error
+    def test_types(self):
+        self.assertRaises(TypeError, is_palindrome, True)
+        self.assertRaises(TypeError, is_palindrome, 15)
+        self.assertRaises(TypeError, is_palindrome, 18.56)
+        self.assertRaises(TypeError, is_palindrome, None)
